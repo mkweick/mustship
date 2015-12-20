f0c7e5238b0c14f2121719a9d0ca58cd0b7d62aa
Matt Weick <mkweick@gmail.com>
2015-12-20 18:15:49 -0500

    logic moved to cron job, update instace vars

--- app/controllers/mustships_controller.rb
+++ app/controllers/mustships_controller.rb
@@ -1,27 +1,8 @@
 class MustshipsController < ApplicationController
-  require 'odbc'
-  CONNECTION = ODBC.connect("mustship", '', '')
 
-  before_action :set_mustships
-
-def index
-    sql = "SELECT MTDISPAT, MTPICKED, MTVERIFY, MTVERFTD from MUSTSHIPTL"
-    stmnt = CONNECTION.prepare(sql)
-    ms_totals = stmnt.execute
-
-    ms_totals.each do |total|
-      @to_be_dispatched = total[0]
-      @to_be_picked = total[1]
-      @to_be_verified = total[2]
-      @total_verified = total[3]
-    end
-
-    stmnt.drop
-  end
-
-  private
-
-  def set_mustships
+  def index
+    @totals = MustshipTotal.first
     @mustships = Mustship.all.order(dispatch_date: :asc, dispatch_time: :asc)
+    @total_mustships = Mustship.count.to_s
   end
 end
