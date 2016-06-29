62db0288997d4f772322871ddb44103583701e14
Matt Weick <mkweick@gmail.com>
2016-06-29 15:52:15 -0400

    refactor scrolling orders

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,23 +21,19 @@ setTimeout(function () {
   location.reload();
 }, 1000 * 60 * 5);
 
-$.fn.infiniteScrollUp=function() {
-  var self=this,kids=self.children()
+$(document).ready(function() {
+  var self = $('section'),
+      kids = self.children();
+
   if (kids.size() > 21) {
     setInterval(function() {
-      kids.slice(21).hide()
-      kids.filter(':hidden').eq(0).slideDown()
+      kids.slice(21).hide();
+      kids.filter(':hidden').eq(0).slideDown();
       kids.eq(0).slideUp(800, "linear",function() {
-        $(this).appendTo(self)
-        kids=self.children()
+        $(this).appendTo(self);
+        kids = self.children();
       })
-    },0)
-		return this;
-  } else {
-    return false;
-  }
-}
-
-$(function(){
-	$('section').infiniteScrollUp()
-})
+    }, 0)
+    return this;
+  };
+});
